467b5a9cec7a1a873ca50d8b5d6fb5c7383a5141
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-06-27 19:19:31 +0430

    linters erros fixed

--- bookList.js
+++ bookList.js
@@ -4,51 +4,51 @@ const author = document.getElementById('author');
 const btn = document.querySelector('.submit');
 
 if (!localStorage.getItem('bookInfo')) {
-    localStorage.setItem('bookInfo', JSON.stringify([]));
+  localStorage.setItem('bookInfo', JSON.stringify([]));
 }
 let books;
 
 function uploadData(book) {
-    localStorage.setItem('bookInfo', JSON.stringify(book));
+  localStorage.setItem('bookInfo', JSON.stringify(book));
 }
 
 function downloadData() {
-    books = JSON.parse(localStorage.getItem('bookInfo'));
-    updateUI();
+  books = JSON.parse(localStorage.getItem('bookInfo'));
+  updateUI();
 }
 downloadData();
 
 btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (titleB.value && author.value) {
-        const obj = { title: titleB.value, author: author.value };
-        books.push(obj);
-        uploadData(books);
-        downloadData();
-        titleB.value = '';
-        author.value = '';
-    }
+  e.preventDefault();
+  if (titleB.value && author.value) {
+    const obj = { title: titleB.value, author: author.value };
+    books.push(obj);
+    uploadData(books);
+    downloadData();
+    titleB.value = '';
+    author.value = '';
+  }
 });
 
 function updateUI() {
-    bookAdd.innerHTML = '';
-    console.log(books);
-    books.forEach((data, index) => {
-        const classBook = document.createElement('div');
-        classBook.classList.add('class-book');
-        const par = document.createElement('p');
-        par.textContent = `${data.title} By ${data.author}`;
-        const btnRemove = document.createElement('button');
-        btnRemove.textContent = `Remove`;
-        btnRemove.addEventListener('click', removeBook.bind(index));
-        classBook.appendChild(par);
-        classBook.appendChild(btnRemove);
-        bookAdd.appendChild(classBook);
-    });
+  bookAdd.innerHTML = '';
+  console.log(books);
+  books.forEach((data, index) => {
+    const classBook = document.createElement('div');
+    classBook.classList.add('class-book');
+    const par = document.createElement('p');
+    par.textContent = `${data.title} By ${data.author}`;
+    const btnRemove = document.createElement('button');
+    btnRemove.textContent = `Remove`;
+    btnRemove.addEventListener('click', removeBook.bind(index));
+    classBook.appendChild(par);
+    classBook.appendChild(btnRemove);
+    bookAdd.appendChild(classBook);
+  });
 }
 
 function removeBook() {
-    books.splice(this, 1);
-    uploadData(books);
-    downloadData();
+  books.splice(this, 1);
+  uploadData(books);
+  downloadData();
 }
