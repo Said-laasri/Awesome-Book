ea7c6127c8904003f12277fb4a608997a294616f
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-06-27 11:15:48 +0100

    Creat variables for functions

---
+++ bookList.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+const bookAdd = document.querySelector('.bookList');// container of all books
+const titleBook = document.querySelector('#title');// input title ID
+const authorBook = document.querySelector('#author');// input author ID
+const btn = document.querySelector('.submit');// class of the Add Button
